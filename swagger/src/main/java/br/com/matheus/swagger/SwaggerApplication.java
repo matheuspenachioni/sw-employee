eca484c09e1f5d5dde226d514d03713947753fec
matheuspenachioni <matheus.penachioni2001@gmail.com>
2023-04-07 18:10:01 -0300

    titulo e descricao da api

--- swagger/src/main/java/br/com/matheus/swagger/SwaggerApplication.java
+++ swagger/src/main/java/br/com/matheus/swagger/SwaggerApplication.java
@@ -3,7 +3,11 @@ package br.com.matheus.swagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger API", description = "REST API for Employee Manager") )
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
